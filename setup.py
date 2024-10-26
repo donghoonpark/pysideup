0788e4589ec79287eb59c157953617bf621944a2
donghoonpark <donghun94@gmail.com>
2024-10-27 07:43:13 +0900

    v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pysideup",
-    version="1.0.0",
+    version="1.0.1",
     author="DongHoon Park",
     author_email="donghun94@gmail.com",
     description="A tool for converting PySide2 code to PySide6",
